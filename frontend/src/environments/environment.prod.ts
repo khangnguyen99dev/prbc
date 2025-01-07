5aa39c63eb112b46f49b033986c039ef7b176591
Khang <ankhangn@visglobalsolutions.com>
2025-01-07 09:14:33 +0700

    build

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
+    production: true,
     api_url: 'https://api.kane-service.com/api',
     site_url: 'https://api.kane-service.com',
     api_token_identifier: 'APIToken@prbc-project'
